7f33697302adbd78664e68c1f1765debeedac405
vipin-edgar <85797853+vipin-edgar@users.noreply.github.com>
2024-12-06 16:19:58 -0500

    v4

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "webserver" {
   instance_type = var.instance_type
   subnet_id     = var.subnet_id
   security_groups = [var.security_group_id]
-  key_name      = var.key_name
+  key_name      = aws_key_pair.example_key.key_name
 
   user_data = var.user_data
 
@@ -11,4 +11,21 @@ resource "aws_instance" "webserver" {
     Name        = "${var.env_name}-Webserver-AZ2"
     Environment = var.env_name
   }
+  # Generate a new private key
+resource "tls_private_key" "example_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# Create the AWS key pair using the public key from the generated private key
+resource "aws_key_pair" "example_key" {
+  key_name   = "example-key"  # Name of the key pair
+  public_key = tls_private_key.example_key.public_key_openssh
+}
+# Save the private key to a local file for SSH access
+resource "local_file" "private_key" {
+  content  = tls_private_key.example_key.private_key_pem
+  filename = "${path.module}/example-key.pem"  # The local path to save the private key
+}
+
 }
