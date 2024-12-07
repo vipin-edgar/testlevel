551361f228cffff262dd7fc4dcbd24b26447ab12
vipin-edgar <85797853+vipin-edgar@users.noreply.github.com>
2024-12-06 19:43:41 -0500

    v12sad

--- modules/compute/outputs.tf
+++ modules/compute/outputs.tf
@@ -9,7 +9,3 @@ output "webserver_public_ip" {
 output "webserver_private_ip" {
   value = aws_instance.webserver.private_ip
 }
-output "private_key_content" {
-  value     = tls_private_key.example_key.private_key_pem
-  sensitive = true
-}
